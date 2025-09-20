55682c5f1a1ab16dab1a6d017cfcfbc9ea0c71ae
Tiep <dhmt254@gmail.com>
2025-09-20 21:31:13 +0700

    Complete authorization for accessing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,53 @@ import ManageCategory from "./pages/ManageCategory/ManageCategory.jsx";
 import ManageUsers from "./pages/ManageUsers/ManageUsers.jsx";
 import ManageItems from "./pages/ManageItems/ManageItems.jsx";
 import Explore from "./pages/Explore/Explore.jsx";
-import {Route, Routes, useLocation} from 'react-router-dom';
+import {Navigate, Route, Routes, useLocation} from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
 import Login from './pages/Login/Login.jsx';
 import OrderHistory from "./pages/OrderHistory/OrderHistory.jsx";
+import {useContext} from "react";
+import {AppContext} from "./context/AppContext.jsx";
 
 const App = () => {
     const location= useLocation();
+
+    const{auth} = useContext(AppContext);
+
+    const LoginRoute = ({element}) => {
+        if (auth.token)
+        {
+            return <Navigate to='/dashboard' replace/>
+        }
+        return element;
+    }
+
+    const ProtectedRoute = ({element,allowedRoles}) => {
+        if (!auth.token)
+        {
+            return <Navigate to='/login' replace/>
+        }
+
+        if (allowedRoles && !allowedRoles.includes(auth.role))
+        {
+            return <Navigate to='/dashboard' replace/>
+        }
+        return element;
+    }
+
     return (
         <div>
             {location.pathname!=="/login" && <Menubar/>}
             <Toaster />
             <Routes>
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/category" element={<ManageCategory />} />
-                <Route path="/users" element={<ManageUsers />} />
-                <Route path="/items" element={<ManageItems />} />
                 <Route path="/explore" element={<Explore />} />
-                <Route path="/login" element={<Login />} />
+
+                {/* Admin Only Routes */}
+                <Route path='/category' element={<ProtectedRoute element={<ManageCategory />} allowedRoles={['ROLE_ADMIN']}/>}/>
+                <Route path='/users' element={<ProtectedRoute element={<ManageUsers />} allowedRoles={['ROLE_ADMIN']}/> }/>
+                <Route path='/items' element={<ProtectedRoute element={<ManageItems />} allowedRoles={['ROLE_ADMIN']}/> }/>
+
+                <Route path='/login' element={<LoginRoute element={<Login/>} />}/>
                 <Route path="/orders" element={<OrderHistory />} />
                 <Route path="/" element={<Dashboard />} />
             </Routes>
--- client/src/components/Menubar/Menubar.jsx
+++ client/src/components/Menubar/Menubar.jsx
@@ -1,11 +1,12 @@
 import './Menubar.css';
 import {assets} from "../../assets/assets.js";
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {AppContext} from "../../context/AppContext.jsx";
 import {useContext} from "react";
 
 const Menubar = () => {
-    const{setAuthData} = useContext(AppContext);
+    const{setAuthData, auth} = useContext(AppContext);
+    const location= useLocation()
     const navigate=useNavigate();
     const logout=()=>{
         localStorage.removeItem('token')
@@ -13,6 +14,12 @@ const Menubar = () => {
         setAuthData(null,null);
         navigate('/login')
     }
+
+    const isActive=(path)=>{
+        return location.pathname===path
+    }
+
+    const isAdmin=auth.role==="ROLE_ADMIN"
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
@@ -26,22 +33,28 @@ const Menubar = () => {
             <div className="collapse navbar-collapse p-2" id="navbarNav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                        <Link className={`nav-link ${isActive('/dashboard') ? 'fw-bold text-warning':''}`} to="/dashboard">Dashboard</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/explore">Explore</Link>
+                        <Link className={`nav-link ${isActive('/explore') ? 'fw-bold text-warning':''}`} to="/explore">Explore</Link>
                     </li>
+                    {
+                        isAdmin && (
+                            <>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${isActive('/items') ? 'fw-bold text-warning':''}`} to="/items">Manage Items</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${isActive('/category') ? 'fw-bold text-warning':''}`} to="/category">Manage Categories</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${isActive('/users') ? 'fw-bold text-warning':''}`} to="/users">Manage Users</Link>
+                                </li>
+                            </>
+                        )
+                    }
                     <li className="nav-item">
-                        <Link className="nav-link" to="/items">Manage Items</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/category">Manage Categories</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/users">Manage Users</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/orders">Order History</Link>
+                        <Link className={`nav-link ${isActive('/orders') ? 'fw-bold text-warning':''}`} to="/orders">Order History</Link>
                     </li>
                 </ul>
                 {/*Add the dropdown for userprofile*/}
